7abada77b4018c2fd77c68e2fac65456e611a54e
강석민 <dol2156@gmail.com>
2023-08-12 01:24:37 +0900

    ㅇㅇ

--- five-server.js
+++ five-server.js
@@ -6,7 +6,7 @@ const option = {
   useLocalIp: true,
   // port: 3000,
   open: true,
-  watch: ['./hbs/', './public/'],
+  watch: ['./hbs', './public', './pub_tool'],
   // wait:1000,
   // injectCss:false,
   // ignore: [/\.s[ac]ss$/i, /\.tsx?$/i],
