5594617c1e237bae44bd0748d46513a293b0bd1f
강석민 <dol2156@gmail.com>
2023-09-15 13:44:17 +0900

    #10

--- assets/lib/tailwindcss.config.js
+++ assets/lib/tailwindcss.config.js
@@ -6,11 +6,12 @@ tailwind.config = {
   theme: {
     extend: {},
     screens: {
-      xs: '0px',
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
+      'xs': '0px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      'pc': '1280px',
       '2xl': '1536px',
       '3xl': '1800px',
     },
