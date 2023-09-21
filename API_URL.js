c7318419fd7ea04f2dbda42abcd21eb05bd3db79
강석민 <dol2156@gmail.com>
2023-09-21 10:38:29 +0900

    배포 시 루트 설정 변경

--- API_URL.js
+++ API_URL.js
@@ -1,3 +1,25 @@
+(() => {
+  function isLocalTestServer() {
+    const hostname = window.location.hostname;
+
+    // hostname이 localhost인지 확인
+    if (hostname === 'localhost') {
+      return true;
+    }
+
+    // hostname이 IP 주소 형식인지 확인
+    const ipPattern = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
+
+    if (ipPattern.test(hostname)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  if(!isLocalTestServer()) window.location.href = `/blank.html`;
+})();
+
 /* https://docs.google.com/spreadsheets/d/1oui5VZpSWpdrpYsHZ9Rwmrv5EVOq3suyndxp4RNDxW8/edit#gid=1038524317 */
 var API_URL = 'https://script.google.com/macros/s/AKfycbyRsrHPlyzuf5mX6Nfob5VJR3IBjbMIG3XxQYnk5iu7U04rmot7-MJ9xHGe5ls96SS_Jw/exec';
 if(typeof module !== "undefined" ){
