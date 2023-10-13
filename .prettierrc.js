4657a2b47d9c1e737beb1214894ffdd437d1db75
강석민 <dol2156@gmail.com>
2023-10-13 13:37:18 +0900

    ddd

--- .prettierrc.js
+++ .prettierrc.js
@@ -42,14 +42,6 @@ module.exports = {
   singleQuote: true,
 
   /**
-   * @template: jsxSingleQuote: <bool>
-   * @description: JSX내에서 큰따옴표("") 대신 작은따옴표('')를 사용여부
-   * true: 홀따옴표로 사용
-   * false: 큰따옴표로 사용
-   */
-  jsxSingleQuote: true,
-
-  /**
    * @template: trailingComma: "<es5|none|all>"
    * @description: 객체나 배열을 작성하여 데이터를 넣을때, 마지막에 후행쉼표를 넣을지 여부
    * es5: 후행쉼표 제외
@@ -57,14 +49,6 @@ module.exports = {
    * all: 후행쉼표 포함
    */
   trailingComma: 'all',
-
-  /**
-   * @template: jsxBracketSameLine: <bool>
-   * @description: ">" 다음 줄에 혼자 있는 대신 여러 줄 JSX 요소를 마지막 줄 끝에 넣습니다
-   * true: 줄넘김하지 않음
-   * false: 줄넘김을 수행
-   */
-  jsxBracketSameLine: true,
 
   /**
    * @template: bracketSpacing: <bool>
